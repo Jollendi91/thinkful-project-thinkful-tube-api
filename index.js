a5eb01304d003691da2522d0ea96dec1aec66187
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-03-13 07:18:19 -0500

    added youtube search functionality

---
+++ index.js
@@ -0,0 +1,41 @@
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+function getDataFromApi(searchTerm, callback) {
+    //get the jSON data from the api
+    const query = {
+        part: 'snippet',
+        q: `${searchTerm}`,
+        key: 'AIzaSyAI0ixIyxcDDXrPDjECt32oim-eMPQR4Uo'
+    }
+    $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+}
+
+function renderResults(result) {
+    return `
+        <div>
+            <img src='${result.snippet.thumbnails.medium.url}'>
+            <h3>${result.snippet.title}</h3>
+            <p>${result.snippet.description}</p>
+        </div>
+    `
+}
+
+function displayYoutubeSearchResults(data) {
+    //render the search thumbnails on the page
+    const results = data.items.map((item, index) => renderResults(item));
+    $('.js-search-results').html(results);
+}
+
+function watchSubmit() {
+    //watch for the form to be submitted
+    $('.js-input-form').submit(event => {
+        event.preventDefault();
+        const queryTarget = $(event.currentTarget).find('.js-search-input');
+        const query = queryTarget.val();
+        queryTarget.val("");
+        getDataFromApi(query, displayYoutubeSearchResults); 
+    }
+    )
+}
+
+$(watchSubmit);
